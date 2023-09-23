5d585eb6f7ae4c6abbc1386ce18680e612ed4cfc
minaylosd <minaylosd@gmail.com>
2023-09-23 15:51:23 +0400

    horizontal scroll

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,48 +1,118 @@
-import { ProductGroup } from "./ProductGroup";
+import { useEffect, useState } from "react";
 
-export const Grid = () => {
+export const Grid = ({ flexLayout, productData }) => {
+  const categories = productData[0].groups;
+
+  function splitGroup(group) {
+    const subgroups = [];
+    for (let i = 0; i < group.products.length; i += 4) {
+      subgroups.push({
+        group: group.group,
+        products: group.products.slice(i, i + 4),
+      });
+    }
+    return subgroups;
+  }
+
+  const newData = categories.flatMap((group) =>
+    group.products.length > 4 ? splitGroup(group) : group
+  );
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const element = document.querySelector(".products__grid");
+    const realWidth = element.scrollWidth;
+    console.log("grid width:", realWidth);
+
+    const scrollInterval = setInterval(() => {
+      setScrollPosition((prevPosition) => {
+        const newPosition = prevPosition + 3840;
+        if (newPosition >= realWidth) {
+          element.scrollTo(0, 0);
+          return 0;
+        } else {
+          element.scrollTo(newPosition, 0);
+          return newPosition;
+        }
+      });
+    }, 10000);
+
+    return () => clearInterval(scrollInterval);
+  }, []);
+
+  const gridStep = {
+    flex: `${flexLayout}`,
+  };
+
   return (
     <>
-      <div className="products__grid">
-        {productData.map((groups) => (
+      <div style={gridStep} className="products__grid">
+        {newData.map((group) => (
           <div key={group.group} className="group">
             <div className="info__wrapper">
-              <div className="column__name">Светлое нефильтрованное</div>
+              <div className="column__name">{group.group}</div>
               <div className="column__name">Цена</div>
               <div className="column__name">Акция</div>
             </div>
-            <div className="product">
-              <div className="column__info">
-                <div className="product__name">Три быка Драфт</div>
-                <div className="product__info">
-                  Алк. 4,5% пл. 12% с. Новая Усмань
+            {group.products.map((product) => (
+              <div className="product" key={product.name}>
+                <div className="column__info">
+                  <div>
+                    <div className="product__name">
+                      {product.name.length > 12
+                        ? `${product.name.substring(0, 12)}...`
+                        : product.name}
+                    </div>
+                    <div className="product__info">
+                      Алк. {product.alcohol}% Пл. {product.density}%{" "}
+                      {product.city}
+                    </div>
+                  </div>
+                  <img
+                    className="product__img"
+                    src={
+                      product.additionally[0].img.src[
+                        "$playback-data-resource"
+                      ] || product.additionally[0].img.src
+                    }
+                  />
                 </div>
+                <div className="column__price">
+                  {product.price}
+                  {product.priceByCard !== 0 && !product.discount.length && (
+                    <div className="stroke"></div>
+                  )}
+                </div>
+                {product.priceByCard !== 0 && !product.discount.length && (
+                  <div className="column__promo">{product.priceByCard}</div>
+                )}
+                {product.discount.length > 0 && (
+                  <div
+                    className={`${
+                      product.discount[0].length > 5
+                        ? "promo__minimised"
+                        : "column__promo"
+                    }`}
+                  >
+                    {product.discount[0]}
+                  </div>
+                )}
               </div>
-              <div className="column__price">141</div>
-              <div className="column__promo">1+1=3</div>
-            </div>
+            ))}
           </div>
         ))}
-        <div className="group">
-          <div className="info__wrapper">
-            <div className="column__name">Светлое нефильтрованное</div>
-            <div className="column__name">Цена</div>
-            <div className="column__name">Акция</div>
-          </div>
-          <div className="product">
-            <div className="column__info">
-              <div className="product__name">Три быка Драфт</div>
-              <div className="product__info">
-                Алк. 4,5% пл. 12% с. Новая Усмань
-              </div>
-            </div>
-            <div className="column__price">141</div>
-            <div className="column__promo">1+1=3</div>
-          </div>
-        </div>
-        <ProductGroup></ProductGroup>
-        <ProductGroup></ProductGroup>
-        <ProductGroup></ProductGroup>
+        {productData[0].additionally[0].img.src === "" ? (
+          <img
+            className="poster"
+            src="https://ae04.alicdn.com/kf/H9a0580ed27b64ef08dfce964d5709475K.jpg"
+          ></img>
+        ) : (
+          <img
+            className="poster"
+            src={productData[0].additionally[0].img.src}
+          ></img>
+        )}
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -30,10 +30,6 @@ function AspectRatioTracker() {
 }
 
 export default function Home({ productData }) {
-  console.log(productData);
-  console.log(productData[0].groups);
-  const categories = productData[0].groups;
-
   const aspectRatio = AspectRatioTracker();
 
   let flexLayout;
@@ -51,44 +47,6 @@ export default function Home({ productData }) {
     // Screens with aspect ratio less than 16:9
     flexLayout = "0 0 33.33%";
   }
-
-  const gridStep = {
-    flex: `${flexLayout}`,
-  };
-
-  // const backgroundImageUrl = productData[0]?.additionally[0]?.img?.src || "";
-
-  // const divStyle = {
-  //   backgroundImage: `url(${backgroundImageUrl})`,
-  //   backgroundSize: "cover",
-  //   backgroundRepeat: "no-repeat",
-  //   height: "100%",
-  // };
-
-  function splitGroup(group) {
-    const subgroups = [];
-    for (let i = 0; i < group.products.length; i += 4) {
-      subgroups.push({
-        group: group.group,
-        products: group.products.slice(i, i + 4),
-      });
-    }
-    return subgroups;
-  }
-
-  const newData = categories.flatMap((group) =>
-    group.products.length > 4 ? splitGroup(group) : group
-  );
-
-  // newData.sort((a, b) => b.products.length - a.products.length);
-
-  console.log(newData);
-
-  useEffect(() => {
-    const element = document.querySelector(".products__grid");
-    const realWidth = element.scrollWidth;
-    console.log("grid width:", realWidth);
-  }, []);
 
   return (
     <>
@@ -109,73 +67,7 @@ export default function Home({ productData }) {
           </>
         )}
       </div>
-      <div style={gridStep} className="products__grid">
-        {newData.map((group) => (
-          <div key={group.group} className="group">
-            <div className="info__wrapper">
-              <div className="column__name">{group.group}</div>
-              <div className="column__name">Цена</div>
-              <div className="column__name">Акция</div>
-            </div>
-            {group.products.map((product) => (
-              <div className="product" key={product.name}>
-                <div className="column__info">
-                  <div>
-                    <div className="product__name">
-                      {product.name.length > 12
-                        ? `${product.name.substring(0, 12)}...`
-                        : product.name}
-                    </div>
-                    <div className="product__info">
-                      Алк. {product.alcohol}% Пл. {product.density}%{" "}
-                      {product.city}
-                    </div>
-                  </div>
-                  <img
-                    className="product__img"
-                    src={
-                      product.additionally[0].img.src[
-                        "$playback-data-resource"
-                      ] || product.additionally[0].img.src
-                    }
-                  />
-                </div>
-                <div className="column__price">
-                  {product.price}
-                  {product.priceByCard !== 0 && !product.discount.length && (
-                    <div className="stroke"></div>
-                  )}
-                </div>
-                {product.priceByCard !== 0 && !product.discount.length && (
-                  <div className="column__promo">{product.priceByCard}</div>
-                )}
-                {product.discount.length > 0 && (
-                  <div
-                    className={`${
-                      product.discount[0].length > 5
-                        ? "promo__minimised"
-                        : "column__promo"
-                    }`}
-                  >
-                    {product.discount[0]}
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
-        {productData[0].additionally[0].img.src === "" ? (
-          <img
-            className="poster"
-            src="https://ae04.alicdn.com/kf/H9a0580ed27b64ef08dfce964d5709475K.jpg"
-          ></img>
-        ) : (
-          <img
-            className="poster"
-            src={productData[0].additionally[0].img.src}
-          ></img>
-        )}
-      </div>
+      <Grid flexLayout={flexLayout} productData={productData}></Grid>
       <div className="footer__div">
         {aspectRatio > 4 && (
           <>
